bf1c822eea95936b5e2c78a58c01a39ecfb4ae3d
VAISHNAVI-R-V <vaishucv14@gmail.com>
2021-05-01 03:02:53 -0700

    added: part time emp wage

---
+++ pt.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+
+IS_PRESENT_FULL_TIME=1
+IS_PRESENT_PART_TIME=2
+WAGE_PER_HR=20
+MAX_WORKING_DAY=20
+
+for((counter=1;counter<= $MAX_WORKING_DAY;counter++))
+do
+employeeCheck=$((Random%3))
+case $employeeCheck in
+    $IS_PREESENT_FULL_TIME)
+empHrs=8;;
+$IS_PRESENT_PART_TIME)
+  empHrs=4;;
+*)
+empHrs=0;;
+esac
+salary=$(($empHrs * $WAGE_PER_HR))
+done
