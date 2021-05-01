fe47769573edca821a483e96b6092ab17041419d
VAISHNAVI-R-V <vaishucv14@gmail.com>
2021-05-01 02:55:50 -0700

    added: daily wage

---
+++ empDaily.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+IS_PRESENT_FULL_TIME=1
+WAGE_PER_HR=20
+
+#VARIABLE
+
+empCheck=$((RANDAM%2))
+
+#SELECTION
+if [ $empCheck -eq $IS_PRESENT_FULL_TIME ]
+then
+empHrs=8
+
+else
+    empHrs=0
+    fi
+ Salary=$(( WAGE_PER_HR * empHrs ))
